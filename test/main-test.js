9197b32c31d5fd9398ebe2a25b68a766797d0de6
jerome-genba <jerome@avoustin.com>
2013-06-12 09:09:36 +0200

    on a un peu avancé sur le pb require

---
+++ test/main-test.js
@@ -0,0 +1,32 @@
+var tests = Object.keys(window.__karma__.files).filter(function (file) {
+    return /\.test\.js$/.test(file);
+});
+
+//require.config({
+//
+//    // !! Testacular serves files from '/base'
+//    baseUrl: '/base/src',
+//    paths: {
+//        require: '../test/lib/require',
+//        chai : '../test/lib/chai'
+//    },
+//    shim: {
+//        require: { exports: "require" },
+//        chai: { exports: "chai" }
+//    }
+//}, ['videogame.test.js'], function() {
+//    window.__karma__.start();
+//});
+
+require({
+    // Testacular serves files from '/  base'
+    baseUrl: '/base/src',
+    paths: {
+        require: '../test/lib/require',
+        chai : '../test/lib/chai'
+    },
+    // ask requirejs to load these files (all our tests)
+    deps: tests,
+    // start test run, once requirejs is done
+    callback: window.__karma__.start
+});
